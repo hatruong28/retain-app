411e7dea289e04c2d029244f4d8b4ee3f8727901
truong.ha <truong.ha@sutrixsolutions.com>
2016-08-25 11:12:56 +0700

    update

---
+++ src/app/containers/notes.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { NoteCard } from '../ui';
+
+@Component({
+  selector: 'notes-container',
+  directives: [NoteCard],
+  template: `
+    <div class="row center-xs notes">
+      <div class="col-xs-6 creator">
+        note creator here
+      </div>
+      <div class="notes col-xs-8">
+        <div class="row between-xs">
+          <note-card
+            class="col-xs-4"
+            [note]="note"
+          >
+          </note-card>
+        </div>
+      </div>
+    </div>
+  `,
+  styles: [`
+    .notes {
+      padding-top: 50px;
+    }
+    .creator {
+      margin-bottom: 40px; 
+    }
+  `]
+})
+export class Notes {
+  note: {title: string, value: string} = {
+    title: 'New note',
+    value: 'New value'
+  };
+}
